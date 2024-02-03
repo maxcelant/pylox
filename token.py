1b4d7558acb5ad0218ef496b7fdfffef91824ced
Max Celant <maxcelant@gmail.com>
2024-02-02 20:00:56 -0600

    token class

---
+++ token.py
@@ -0,0 +1,9 @@
+class Token:
+  def __init__(self, token_type: TokenType, lexeme: str, literal: object, line: int):
+    self.token_type = token_type
+    self.lexem = lexeme
+    self.literal = literal
+    self.line = line
+
+  def __str__(self):
+    return f'{self.token_type} {lexeme} {literal}'
