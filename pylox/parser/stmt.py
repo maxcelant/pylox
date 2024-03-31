cebc91a05a8c68323c9fb66a308369e491ec928c
Max Celant <maxcelant@gmail.com>
2024-03-31 12:08:57 -0500

    fix spacing

--- pylox/parser/stmt.py
+++ pylox/parser/stmt.py
@@ -16,20 +16,18 @@ class Stmt(ABC):
     def visit_print_stmt(self, expr: Stmt.Expr):
       pass
 
-
     @abstractmethod
     def visit_expression_stmt(self, expr: Stmt.Expr):
       pass
-
 
     @abstractmethod
     def visit_var_stmt(self, expr: Stmt.Expr):
       pass
 
+
   class Expression:
     def __init__(self, expression: Expr):
       self.expression = expression
-
 
     def accept(self, visitor: Stmt.Visitor):
       visitor.visit_expression_stmt(self)
@@ -42,19 +40,18 @@ class Stmt(ABC):
     def __init__(self, expression: Expr):
       self.expression = expression
 
-
     def accept(self, visitor: Stmt.Visitor):
       visitor.visit_print_stmt(self)
 
     def __repr__(self):
       return f'Print({self.expression=})'
 
+
   class Var:
     def __init__(self, name: TokenItem, initializer: Expr):
       self.name = name
       self.initializer = initializer
 
-    
     def accept(self, visitor: Stmt.Visitor):
       visitor.visit_var_stmt(self)
 
